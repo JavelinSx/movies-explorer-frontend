83df62d18143dd988f53f252fb2b256f5596ec9a
JavelinSx <zuev499@gmail.com>
2022-12-30 08:45:12 +0300

    add search hover repair

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,12 +2,15 @@ import './SearchForm.css';
 import FilterCheckBox from '../FilterCheckBox/FilterCheckBox';
 import iconSearch from '../../images/search-icon.svg'
 function SearchForm(){
+    const handleSearch = (evt) => {
+        evt.preventDefault()
+    }
     return(
         <div className='search-container'>
-            <form className='search-form'>
+            <form className='search-form'onSubmit={handleSearch}>
                 <label className='search-input-label' htmlFor='input-search'>
                     <img className='search-input__icon-search' src={iconSearch} alt='иконка лупы'></img>
-                    <input className='search-input' placeholder='Фильм' id='input-search'>
+                    <input className='search-input' placeholder='Фильм' id='input-search' required>
                         
                     </input>
                     <button className='search-btn'>
